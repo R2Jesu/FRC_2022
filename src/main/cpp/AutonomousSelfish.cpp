b2fde7fcb105671669fe42a4eaa9b78b9c21eddd
R2Jesu <bhilton@corjesu.org>
2022-09-22 22:01:25 -0500

    final_GRC_updates

--- src/main/cpp/AutonomousSelfish.cpp
+++ src/main/cpp/AutonomousSelfish.cpp
@@ -7,21 +7,21 @@ void Robot::R2Jesu_AutonomousSelfish(void)
     std::this_thread::sleep_for(std::chrono::seconds(1));
     while (m_DriveEncoder1.GetPosition() < 40.0 && frc::DriverStation::IsAutonomousEnabled())
     {
-        m_SwerveDrive1.Set(autoSpeed);
         pidOutput1 = m_angleController1.Calculate((m_SwerveAnalog1.GetVoltage() * conversion), 90.0);
         m_SwerveTurn1.Set(pidOutput1);
+        m_SwerveDrive1.Set(autoSpeed);
 
-        m_SwerveDrive2.Set(autoSpeed);
         pidOutput2 = m_angleController2.Calculate((m_SwerveAnalog2.GetVoltage() * conversion), 87.0);
         m_SwerveTurn2.Set(pidOutput2);
+        m_SwerveDrive2.Set(autoSpeed);
 
-        m_SwerveDrive3.Set(autoSpeed);
         pidOutput3 = m_angleController3.Calculate((m_SwerveAnalog3.GetVoltage() * conversion), 87.0);
         m_SwerveTurn3.Set(pidOutput3);
+        m_SwerveDrive3.Set(autoSpeed);
 
-        m_SwerveDrive4.Set(autoSpeed);
         pidOutput4 = m_angleController4.Calculate((m_SwerveAnalog4.GetVoltage() * conversion), 90.0);
         m_SwerveTurn4.Set(pidOutput4);
+        m_SwerveDrive4.Set(autoSpeed);
         //frc::SmartDashboard::PutNumber("position", m_DriveEncoder1.GetPosition());
     }
     m_SwerveDrive1.Set(0.0);
@@ -37,25 +37,26 @@ void Robot::R2Jesu_AutonomousSelfish(void)
     m_DriveEncoder3.SetPosition(0.0);
     m_DriveEncoder4.SetPosition(0.0);
     std::this_thread::sleep_for(std::chrono::seconds(1));
+    m_intake.Set(0);
     intakePneumatics.Set(frc::DoubleSolenoid::Value::kReverse);
 
-    while (m_DriveEncoder1.GetPosition() < 60.0 && frc::DriverStation::IsAutonomousEnabled())
+    while (m_DriveEncoder1.GetPosition() < 55.0 && frc::DriverStation::IsAutonomousEnabled())
     {
-        m_SwerveDrive1.Set(autoSpeed);
         pidOutput1 = m_angleController1.Calculate((m_SwerveAnalog1.GetVoltage() * conversion), 270.0);
         m_SwerveTurn1.Set(pidOutput1);
+        m_SwerveDrive1.Set(autoSpeed);
 
-        m_SwerveDrive2.Set(autoSpeed);
-        pidOutput2 = m_angleController2.Calculate((m_SwerveAnalog2.GetVoltage() * conversion), 270.0);
+        pidOutput2 = m_angleController2.Calculate((m_SwerveAnalog2.GetVoltage() * conversion), 265.0); //260
         m_SwerveTurn2.Set(pidOutput2);
+        m_SwerveDrive2.Set(autoSpeed);
 
-        m_SwerveDrive3.Set(autoSpeed);
-        pidOutput3 = m_angleController3.Calculate((m_SwerveAnalog3.GetVoltage() * conversion), 270.0);
+        pidOutput3 = m_angleController3.Calculate((m_SwerveAnalog3.GetVoltage() * conversion), 265.0); //260
         m_SwerveTurn3.Set(pidOutput3);
+        m_SwerveDrive3.Set(autoSpeed);
 
-        m_SwerveDrive4.Set(autoSpeed);
         pidOutput4 = m_angleController4.Calculate((m_SwerveAnalog4.GetVoltage() * conversion), 270.0);
         m_SwerveTurn4.Set(pidOutput4);
+        m_SwerveDrive4.Set(autoSpeed);
 
     }
 
@@ -73,22 +74,22 @@ void Robot::R2Jesu_AutonomousSelfish(void)
     m_DriveEncoder4.SetPosition(0.0);
     std::this_thread::sleep_for(std::chrono::seconds(1));
 
-    /*while (m_DriveEncoder1.GetPosition() < 42.0 && frc::DriverStation::IsAutonomousEnabled())
+    while (m_DriveEncoder1.GetPosition() < 42.0 && frc::DriverStation::IsAutonomousEnabled())
     {
         m_SwerveDrive1.Set(autoSpeed);
-        pidOutput1 = m_angleController1.Calculate((m_SwerveAnalog1.GetVoltage() * conversion), 269.0);
+        pidOutput1 = m_angleController1.Calculate((m_SwerveAnalog1.GetVoltage() * conversion), 270.0);
         m_SwerveTurn1.Set(pidOutput1);
 
         m_SwerveDrive2.Set(autoSpeed);
-        pidOutput2 = m_angleController2.Calculate((m_SwerveAnalog2.GetVoltage() * conversion), 270.0);
+        pidOutput2 = m_angleController2.Calculate((m_SwerveAnalog2.GetVoltage() * conversion), 275.0);
         m_SwerveTurn2.Set(pidOutput2);
 
         m_SwerveDrive3.Set(autoSpeed);
-        pidOutput3 = m_angleController3.Calculate((m_SwerveAnalog3.GetVoltage() * conversion), 270.0);
+        pidOutput3 = m_angleController3.Calculate((m_SwerveAnalog3.GetVoltage() * conversion), 275.0);
         m_SwerveTurn3.Set(pidOutput3);
 
         m_SwerveDrive4.Set(autoSpeed);
-        pidOutput4 = m_angleController4.Calculate((m_SwerveAnalog4.GetVoltage() * conversion), 269.0);
+        pidOutput4 = m_angleController4.Calculate((m_SwerveAnalog4.GetVoltage() * conversion), 270.0);
         m_SwerveTurn4.Set(pidOutput4);
 
     }
@@ -114,7 +115,7 @@ void Robot::R2Jesu_AutonomousSelfish(void)
         shSpeed = shSpeed + 0.0001;
         m_shooter.Set(shSpeed);
         //printf("velo in while %f", m_shooterEncoder.GetVelocity());
-    }*/
+    }
     m_indexer1.Set(1.0);
     m_indexer2.Set(1.0);
     
